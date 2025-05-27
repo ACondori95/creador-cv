3804769962ae4c38c320419643e1a70c34ccd8a3
Álvaro Condorí <condorialvaro95@gmail.com>
2025-05-27 12:25:58 -0300

    Defining App Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Dashboard from "./pages/Home/Dashboard";
+import EditResume from "./pages/ResumeUpdate/EditResume";
+import {Toaster} from "react-hot-toast";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div>
+        <Router>
+          <Routes>
+            {/* Default Route */}
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/ingresa' element={<Login />} />
+            <Route path='/registrate' element={<SignUp />} />
+            <Route path='/inicio' element={<Dashboard />} />
+            <Route path='/cv/:resumeId' element={<EditResume />} />
+          </Routes>
+        </Router>
+      </div>
+
+      <Toaster toastOptions={{className: "", style: {fontSize: "13px"}}} />
+    </>
+  );
 };
 
 export default App;
