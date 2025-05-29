ac55a8e57dff5a9fdaec218cfa4562e77479a59e
Álvaro Condorí <condorialvaro95@gmail.com>
2025-05-29 19:33:17 -0300

    Integrating Sign-Up API

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,6 +7,7 @@ const {
 } = require("../controllers/authController");
 const cloudinary = require("../utils/cloudinary");
 const upload = require("../middlewares/uploadMiddleware");
+const streamifier = require("streamifier");
 
 const router = express.Router();
 
@@ -16,16 +17,28 @@ router.post("/login", loginUser); // Login User
 router.get("/profile", protect, getUserProfile); // Get User Profile
 
 router.post("/upload-image", upload.single("image"), (req, res) => {
-  cloudinary.uploader.upload(req.file.path, function (err, result) {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({success: false, message: "Error"});
+  if (!req.file) {
+    return res
+      .status(400)
+      .json({success: false, message: "No se envió ninguna imagen"});
+  }
+
+  const stream = cloudinary.uploader.upload_stream((error, result) => {
+    if (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({success: false, message: "Error al subir la imagen"});
     }
 
-    res
-      .status(200)
-      .json({success: true, message: "Imagen subida con éxito", data: result});
+    res.status(200).json({
+      success: true,
+      message: "Imagen subida con éxito",
+      data: result,
+    });
   });
+
+  streamifier.createReadStream(req.file.buffer).pipe(stream);
 });
 
 module.exports = router;
--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,8 +1,12 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {validateEmail} from "../../utils/helper";
 import Input from "../../components/Inputs/Input";
 import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
+import axiosInstance from "../../utils/axiosInstance";
+import {API_PATHS} from "../../utils/apiPaths";
+import {UserContext} from "../../context/userContext";
+import uploadImage from "../../utils/uploadImage";
 
 const SignUp = ({setCurrentPage}) => {
   const [profilePic, setProfilePic] = useState(null);
@@ -12,6 +16,7 @@ const SignUp = ({setCurrentPage}) => {
 
   const [error, setError] = useState(null);
 
+  const {updateUser} = useContext(UserContext);
   const navigate = useNavigate();
 
   // Handle SignUp From Submit
@@ -39,7 +44,33 @@ const SignUp = ({setCurrentPage}) => {
 
     // SignUp API Call
     try {
-    } catch (error) {}
+      // Upload image if present
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        name: fullName,
+        email,
+        password,
+        profileImageUrl,
+      });
+
+      const {token} = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data);
+        navigate("/inicio");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Algo salió mal. Por favor, intentá de nuevo.");
+      }
+    }
   };
 
   return (
